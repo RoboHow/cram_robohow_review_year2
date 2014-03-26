e82a5bc368515b5fca6267f6321b1b3c77351eba
Jan Winkler <winkler@cs.uni-bremen.de>
2014-03-26 16:42:16 +0100

    Hotfix: Wrong package for semantic map name

--- src/sem-map-config.lisp
+++ src/sem-map-config.lisp
@@ -25,7 +25,7 @@
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :cram-robohow-review-year2)
+(in-package :sem-map-utils)
 
 (def-fact-group semantic-map-data (semantic-map-name)
 
